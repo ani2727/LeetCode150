095d736d549b1fb6012cbb0da6256f11971788ae
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-06-24 15:32:02 +0530

    Create FindTheIndexofFirstOccurence.java

---
+++ FindTheIndexofFirstOccurence.java
@@ -0,0 +1,10 @@
+//Problem Link: https://rb.gy/pafpd5
+
+//code
+class Solution 
+{
+    public int strStr(String haystack, String needle) 
+    {
+        return haystack.indexOf(needle);
+    }
+}
